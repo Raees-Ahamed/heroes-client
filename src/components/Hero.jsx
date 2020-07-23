39669ae685cddb12714d79d5a26d768d54615543
Raees-Ahamed-Samman <raees@inexisconsulting.com>
2020-07-23 17:44:14 +0530

    updated hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,34 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-class Hero extends Component{
-    state = {
-        heroId : 100
-    };
-    render(){
+class Hero extends Component {
+  state = {
+    heroId: 100,
+  };
+  render() {
+    //we can use a <div> or <React.Fragment> as below
+    // return (<diV>
+    //         <h1>Avengers Incoming.....</h1><br/>
+    //         <button class='btn btn-warning'>Add Hero</button>
+    //     </diV>
+    // );
 
-        //we can use a <div> or <React.Fragment> as below 
-        // return (<diV>
-        //         <h1>Avengers Incoming.....</h1><br/>
-        //         <button class='btn btn-warning'>Add Hero</button>               
-        //     </diV>
-        // );
-
-        return (
-            <React.Fragment>               
-                <div class="container">
-                    <div class="form">
-                        <h1>Avengers Incoming.....</h1><br/>
-                        <input type="text" class="form-control form-control-sm"></input><br/>
-                        <label>hero Id : {this.state.heroId}</label><br/>
-                        <button class='btn btn-warning'>Add Hero</button>
-                    </div>
-                </div>               
-            </React.Fragment>
-        )
-    }
+    return (
+      <React.Fragment>
+        <div class="container">
+          <div class="form">
+            <h1>Avengers Incoming.....</h1>
+            <br />
+            <input type="text" class="form-control form-control-sm"></input>
+            <br />
+            <label>hero Id : {this.state.heroId}</label>
+            <br />
+            <button class="btn btn-warning">Add Hero</button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Hero;
